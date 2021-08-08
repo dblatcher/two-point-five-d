2cedcacfbcb64647d6cdc3b135fc93fe097bd765
David Blatcher <david.blatcher@graduate-jobs.com>
2021-08-08 18:07:55 +0100

    better arrow for PlayerVantage

--- src/game-classes/PlayerVantage.ts
+++ src/game-classes/PlayerVantage.ts
@@ -1,4 +1,5 @@
-import { PlotConfig, plotPolygon } from "@/canvas/canvas-utility";
+import { PlotConfig, plotPolygon, Point } from "@/canvas/canvas-utility";
+import { Color } from "@/canvas/Color";
 import { Direction } from "./Direction";
 import { Vantage } from "./Vantage";
 
@@ -16,7 +17,19 @@ class PlayerVantage extends Vantage {
         this.data = config
     }
 
-    drawInMapConfig:PlotConfig = { noClose: true, noFill:true, strokeStyle:'red' }
+    drawInMapConfig: PlotConfig = { noClose: false, noFill: false, fillStyle: Color.RED.css, strokeStyle: Color.BLACK.css }
+
+    get drawInMapPoints(): Point[][] {
+        const { direction: d } = this.data;
+        const origin = { x: 0, y: 0 }
+        const arrowEnd = d.translatePoint(origin, .4)
+        let arrowLeft = d.leftOf.translatePoint(origin,.2);
+        arrowLeft = d.behind.translatePoint(arrowLeft,.1)
+        let arrowRight = d.rightOf.translatePoint(origin,.2);
+        arrowRight = d.behind.translatePoint(arrowRight,.1)
+        return [[arrowEnd, arrowLeft, origin, arrowRight]]
+    }
+
 }
 
-export { PlayerVantage, PlayerVantageConfig}
+export { PlayerVantage, PlayerVantageConfig }
