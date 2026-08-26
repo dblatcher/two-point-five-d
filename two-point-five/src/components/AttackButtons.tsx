418893c4a2fce3c389548d59aac6dab6d092b322
DBlatcher <Dv.bltchr@gmail.com>
2026-08-26 07:40:21 +0100

    show attack result

--- two-point-five/src/components/AttackButtons.tsx
+++ two-point-five/src/components/AttackButtons.tsx
@@ -2,6 +2,7 @@ import { useState } from "react"
 import { useGame } from "./GameContext"
 import { FeedbackToUI } from "@/game-classes/Game"
 import { ItemSlot } from "./ItemSlot"
+import { AttackOption } from "@/rpg-classes/AttackOption"
 
 
 
@@ -21,47 +22,59 @@ const CharacterAttackButtons = ({ charcterIndex }: { charcterIndex: number }) =>
         return <div></div>
     }
 
+    const doAttack = (option: AttackOption) => {
+        const result = game().handleAttackButton({
+            character,
+            option
+        })
+        flashFeedBack(result)
+    }
+
+    const damageDone = feedback?.propertyList?.find(item => item[0] === 'damage')?.[1];
 
     return <div style={{
         backgroundColor: character.getMyColor(game()).lighter(100).css,
         flex: 1
     }}>
-        <ItemSlot
-            getItem={() => {
-                const item = character.data.equipmentSlots?.get('RIGHT_HAND');
-                return item && item.data.type.isWieldable ? item : undefined
-            }}
-            style={{
-                filter: "brightness(0)"
-            }}
-        />
+        <div style={{
+            position: 'relative'
+        }}>
+            <ItemSlot
+                getItem={() => {
+                    const item = character.data.equipmentSlots?.get('RIGHT_HAND');
+                    return item && item.data.type.isWieldable ? item : undefined
+                }}
+                style={{
+                    filter: "brightness(0)"
+                }}
+            />
+            {feedback && (
+                <span style={{
+                    position:'absolute',
+                    left:'50%',
+                    top:'50%',
+                    transform: 'translateX(-50%) translateY(-50%)',
+                    background: 'white',
+                    color: 'black'
+                }}>
+                    {damageDone ?? 'miss'}
+                </span>
+            )}
+        </div>
         <div style={{ display: 'flex', flexDirection: 'column' }}>
             {character.attackOptions.map((option, index) =>
                 <button key={index}
-                    disabled={(character.attackCooldown ?? 0) > 0}
-                    onClick={() => {
-
-                        if (!character) {
-                            return
-                        }
-                        const result = game().handleAttackButton({
-                            character,
-                            option
-                        })
-                        flashFeedBack(result)
-                    }}
+                    disabled={(character.attackCooldown ?? 0) > 0 || !character.canAct}
+                    onClick={() => doAttack(option)}
                 >{option.data.name}</button>
             )}
         </div>
-        <div>{feedback?.message}</div>
     </div>
 }
 
 
 export const AttackButtons = () => {
-
     const { gameData } = useGame()
-
     return (
         <section style={{ display: 'flex', gap: 2, justifyContent: 'space-between' }}>
             {gameData.characters.map((_, index) => (
