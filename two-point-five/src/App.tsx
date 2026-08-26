1f435a692aa6de607d18d30137ced063386c72df
DBlatcher <Dv.bltchr@gmail.com>
2026-08-26 21:33:59 +0100

    game runner hook

--- two-point-five/src/App.tsx
+++ two-point-five/src/App.tsx
@@ -1,50 +1,12 @@
-import { useCallback, useEffect, useRef, useState } from 'react'
 // import './App.css'
 import { game } from '@/travels-in-generica'
 import { GameContext } from './components/GameContext'
 import { RpgGameLayout } from './components/RpgGameLayout'
+import { useGameRunner } from './useGameRunner'
 
 function App() {
 
-  const gameRef = useRef(game)
-  const [gameData, setGameData] = useState(gameRef.current.data)
-  const [canvas, setCanvas] = useState<HTMLCanvasElement | null>(null)
-  const [ready, setReady] = useState(false)
-
-  const renderSight = useCallback(() => {
-    if (canvas) {
-      const { playerVantage, level } = gameRef.current.data;
-      level.drawAsSight(canvas, playerVantage)
-    }
-  }, [canvas])
-
-
-
-  useEffect(() => {
-    if (ready) {
-      return
-    }
-    gameRef.current.loadImages().then(() => {
-      setReady(true)
-      renderSight()
-    })
-  }, [renderSight, ready])
-
-  useEffect(() => {
-    const runTick = () => {
-      if (!ready) {
-        return
-      }
-      gameRef.current.tick()
-      renderSight()
-      setGameData({ ...gameRef.current.data })
-    }
-    const interval = setInterval(runTick, 100)
-    return () => {
-      clearInterval(interval)
-    }
-  }, [renderSight, ready])
-
+  const { ready, gameData, gameRef, setCanvas, canvas } = useGameRunner(game)
 
   if (!ready) {
     return null
---
+++ two-point-five/src/useGameRunner.ts
@@ -0,0 +1,47 @@
+import { useCallback, useEffect, useRef, useState } from "react"
+import { Game } from "./game-classes/Game"
+
+
+export const useGameRunner = (game: Game) => {
+    const gameRef = useRef(game)
+    const [gameData, setGameData] = useState(gameRef.current.data)
+    const [canvas, setCanvas] = useState<HTMLCanvasElement | null>(null)
+    const [ready, setReady] = useState(false)
+
+    const renderSight = useCallback(() => {
+        if (canvas) {
+            const { playerVantage, level } = gameRef.current.data;
+            level.drawAsSight(canvas, playerVantage)
+        }
+    }, [canvas])
+
+
+    useEffect(() => {
+        if (ready) {
+            return
+        }
+        gameRef.current.loadImages().then(() => {
+            setReady(true)
+            renderSight()
+        })
+    }, [renderSight, ready])
+
+    useEffect(() => {
+        const runTick = () => {
+            if (!ready) {
+                return
+            }
+            gameRef.current.tick()
+            renderSight()
+            setGameData({ ...gameRef.current.data })
+        }
+        const interval = setInterval(runTick, 100)
+        return () => {
+            clearInterval(interval)
+        }
+    }, [renderSight, ready])
+
+    return {
+        ready, canvas, setCanvas, gameData, gameRef
+    }
+}
